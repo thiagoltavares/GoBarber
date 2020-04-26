449c737365b379713ca5af0177150eba38945b95
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-04-26 15:34:32 +0100

    Input usability, onfocus, onblur effects

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,36 +9,42 @@ import { Container, Content, Background } from './styles';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-const SignUp: React.FC = () => (
-  <>
-    <Container>
-      <Background />
-      <Content>
-        <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-        <form>
-          <h1>Sign Up</h1>
+  return (
+    <>
+      <Container>
+        <Background />
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
 
-          <Input name="name" icon={FiUser} placeholder="Name" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Form onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
 
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="password"
-          />
+            <Input name="name" icon={FiUser} placeholder="Name" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Button type="submit">Sign Up</Button>
-        </form>
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="password"
+            />
 
-        <a href="login">
-          <FiArrowLeft />
-          Back
-        </a>
-      </Content>
-    </Container>
-  </>
-);
+            <Button type="submit">Sign Up</Button>
+          </Form>
+
+          <a href="login">
+            <FiArrowLeft />
+            Back
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
 
 export default SignUp;
