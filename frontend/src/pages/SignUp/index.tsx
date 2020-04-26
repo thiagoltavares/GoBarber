67925c93e6bbcdcce0a2365e700ebd919652144f
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-04-26 16:33:29 +0100

    Validated wit yup and using error handling

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,49 +1,70 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
-
-import { Container, Content, Background } from './styles';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
+import { Container, Content, Background } from './styles';
+
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Enter your name'),
+        email: Yup.string()
+          .required('Enter your e-mail')
+          .email('Insert a valid format'),
+        password: Yup.string().min(6, 'At least 6 characters'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
-    <>
-      <Container>
-        <Background />
-        <Content>
-          <img src={logoImg} alt="GoBarber" />
+    <Container>
+      <Background />
 
-          <Form onSubmit={handleSubmit}>
-            <h1>Sign Up</h1>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-            <Input name="name" icon={FiUser} placeholder="Name" />
-            <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Sign Up</h1>
 
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="password"
-            />
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-            <Button type="submit">Sign Up</Button>
-          </Form>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="password"
+          />
 
-          <a href="login">
-            <FiArrowLeft />
-            Back
-          </a>
-        </Content>
-      </Container>
-    </>
+          <Button type="submit">Sign Up</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Back
+        </a>
+      </Content>
+    </Container>
   );
 };
 
