839029d5a95ef45656918b1a27bff1dda42d01c8
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-04-26 10:32:40 +0100

    button and input components isolated

---
+++ frontend/src/components/Input/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      -webkit-transition: 'color 9999s ease-out, background-color 9999s ease-out';
+      -webkit-transition-delay: 9999s;
+    }
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+    color: #666360;
+  }
+`;
--- frontend/src/pages/sigin/styles.ts
+++ frontend/src/pages/sigin/styles.ts
@@ -28,41 +28,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      padding: 16px;
-      width: 100%;
-      color: #f4ede8;
-
-      &::placeholder {
-        color: #666360;
-      }
-
-      & + input {
-        margin-top: 8px;
-      }
-    }
-
-    button {
-      background: #ff9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      color: #312e38;
-      width: 100%;
-      font-weight: 500;
-      margin-top: 16px;
-
-      transition: background-color 0.2s;
-
-      &:hover {
-        background-color: ${shade(0.2, '#FF9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
@@ -82,6 +47,13 @@ export const Content = styled.div`
     margin-top: 24px;
     text-decoration: none;
     transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
 
     &:hover {
       color: ${shade(0.2, '#FF9000')};
