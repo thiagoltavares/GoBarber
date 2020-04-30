5dde8f0e7bd43d628b9e585b4de5a1c9b01c8e13
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-04-30 09:26:46 +0100

    input and button components and styles

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
 import { Container, Title } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import logImg from '../../assets/logo.png';
 
@@ -8,7 +10,20 @@ const SigIn: React.FC = () => {
   return (
     <Container>
       <Image source={logImg} />
+
       <Title>Log on</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+
+      <Input name="password" icon="lock" placeholder="password" />
+
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
