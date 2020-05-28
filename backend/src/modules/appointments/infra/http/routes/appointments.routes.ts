b4d867e521e9592f3f2a85356346e8d52d3e3546
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-05-28 19:21:47 +0100

    appointment schedule

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import ensureAthenticated from '@modules/users/infra/http/middlewares/ensureAthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentRouter.use(ensureAthenticated);
 
 appointmentRouter.post('/', appointmentsController.create);
+
+appointmentRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentRouter;
