cab39378d8f45af519619f464221fd1ffb0cd6e3
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-05-29 07:37:57 +0100

    refator tests

---
+++ backend/src/modules/notifications/repositories/fakes/FakeNotificationRepository.ts
@@ -0,0 +1,25 @@
+import { ObjectID } from 'mongodb';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+
+import Notification from '@modules/notifications/infra/typeorm/schemas/Notification';
+
+class FakeNotificationRepository implements INotificationsRepository {
+  private notifications: Notification[] = [];
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = new Notification();
+
+    Object.assign(notification, { content, recipient_id, id: new ObjectID() });
+
+    this.notifications.push(notification);
+
+    return notification;
+  }
+}
+
+export default FakeNotificationRepository;
