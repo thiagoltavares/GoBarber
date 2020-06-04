d531b75c553df7b16b0c866d8122d4956f5446d9
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-06-04 09:14:01 +0100

    brute force prevent with rate limiter flexible

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -9,12 +9,14 @@ import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm/';
 import '@shared/container';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 
 import routes from './routes';
 
 const app = express();
 app.use(cors());
 
+app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
