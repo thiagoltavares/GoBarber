1d0c6e0d421e725db3961ccb3c9f3a86e0fc2f6d
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-06-17 07:38:58 +0100

    list appointment frontend

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,70 @@
 import React from 'react';
 
+import { FiPower, FiLock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="gobarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt="avatar" />
+            <div>
+              <span>Welcome,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/43707021?s=460&u=01d71a26945e6fb6542ae3ca0e8f3a9c5922b70f&v=4"
+                alt="thiago tavares"
+              />
+              <strong>Thiago tavares</strong>
+              <span>
+                <FiLock />
+                8:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
 };
 
 export default Dashboard;
