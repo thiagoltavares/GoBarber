ddf6f8bca4704c4066b1f72afd434ce92f8dd33b
Thiago Tavares <thiago.tavares@present-technologies.com>
2020-07-20 07:21:10 +0100

    Button and input

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -5,11 +5,18 @@ import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
+// Components
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="password" />
+      <Button onPress={() => console.log('Clickou')}>Entrar</Button>
     </Container>
   );
 };
